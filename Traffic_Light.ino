6389302499edcd0af8d0fa2f8563bf7c69917454
unknown <jblack@byu.edu>
2019-09-19 22:12:49 -0600

    Added functions for each light.

--- Traffic_Light.ino
+++ Traffic_Light.ino
@@ -1,34 +1,58 @@
 
+#include <ESP8266WiFi.h>
+
 int redLED = D6;
 int yellowLED = D7;
 int greenLED = D8;
 
+void greenLight() {
+  digitalWrite(redLED, LOW);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(greenLED, HIGH);
+}
+void yellowLight() {
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, LOW);
+  digitalWrite(yellowLED, HIGH);
+}
+
+void redLight() {
+  digitalWrite(greenLED, LOW);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(redLED, HIGH);
+}
+
+void offLight() {
+  digitalWrite(greenLED, LOW);
+  digitalWrite(yellowLED, LOW);
+  digitalWrite(redLED, LOW);
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.print("Serial Output Beginning.");
+  Serial.println("Serial Output Beginning.");
   pinMode(redLED, OUTPUT);
   pinMode(yellowLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   digitalWrite(redLED, LOW);
   digitalWrite(yellowLED, LOW);
   digitalWrite(greenLED, LOW);
-  Serial.print("Done Initializing LEDs.");
+  Serial.println("Done Initializing LEDs.");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Red");
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(redLED,HIGH);
+  Serial.println("Red");
+  redLight();
   delay(5000);
-  Serial.print("Green");
-  digitalWrite(redLED,LOW);
-  digitalWrite(greenLED,HIGH);
+  Serial.println("Green");
+  greenLight();
   delay(5000);
-  Serial.print("Yellow");
-  digitalWrite(greenLED,LOW);
-  digitalWrite(yellowLED,HIGH);
+  Serial.println("Yellow");
+  yellowLight();
   delay(5000);
 }
