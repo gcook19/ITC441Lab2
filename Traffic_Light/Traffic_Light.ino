322c48bdc82b35fad3f8226a02407ced1b76bf73
unknown <jblack@byu.edu>
2019-09-19 22:34:59 -0600

    Added programing for Auto Mode. Started code for Manual Mode.

--- Traffic_Light/Traffic_Light.ino
+++ Traffic_Light/Traffic_Light.ino
@@ -13,6 +13,7 @@ bool automode = false;
 const char* ssid = "WIFI NAME";
 const char* password = "WIFI PASSWORD";
 unsigned long currentMillis;
+unsigned long nextStage;
 WiFiServer server(80);
 int curState;
 Queue_t stateQueue;
@@ -73,14 +74,14 @@ void setup() {
   q_init(&stateQueue, sizeof(int), 20, FIFO, false);
   Serial.println("Queue created...");
   currentMillis = millis();
-  startMillis = currentMillis - 1;
+  nextStage = 0;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("Red");
-  currentMillis = millis();
+ 
   WiFiClient client = server.available();
   if(client) {
     while (!client.available()) {
@@ -88,10 +89,11 @@ void loop() {
     }
   }
 
-  String request = client.readStringUntil('\r');
+  //String request = client.readStringUntil('\r');
+  client.setTimeout(100);
   Serial.println(request);
   client.flush();
-
+  //Parse and deal with input.
   if(request.indexOf("/Off") != -1) {
     automode = false;
     //Queuing up this request
@@ -99,7 +101,7 @@ void loop() {
     Serial.println("Queued Off State.");
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("")
+    client.println("");
   }
   
   if(request.indexOf("/Green") != -1) {
@@ -109,7 +111,7 @@ void loop() {
     Serial.println("Queued Green State.");
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("")
+    client.println("");
   }
   else if(request.indexOf("/Red") != -1) {
     automode = false;
@@ -118,7 +120,7 @@ void loop() {
     Serial.println("Queued Red State.");
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("")
+    client.println("");
   }
   else if(request.indexOf("/Yellow") != -1) {
     automode = false;
@@ -127,14 +129,31 @@ void loop() {
     Serial.println("Queued Yellow State.");
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("")
+    client.println("");
   }
   else if(request.indexOf("/Status") != -1) {
     //We need to print out the status here.
     Serial.println("Request for Status.");
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("")  
+    client.println("");
+    client.print("{\"mode\":\"");
+    if(automode) {
+      client.print("auto");
+    } else {
+      client.print("manual");  
+    }
+    client.print("\",\"light\":\");
+    if(curState == redState) {
+      client.print("red");
+    } else if (curState == greenState) {
+      client.print("green");
+    } else if (curState == yellowState) {
+      client.print("yellow");
+    } else {
+      client.print("off");
+    }
+    client.println("\"}");
   }
   else if(request.indexOf("/Auto") != -1) {
     automode = true;
@@ -143,9 +162,39 @@ void loop() {
     Serial.println("Auto Mode.");
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("")
+    client.println("");
     
+  } else {
+    //I need to output a web page here.
   }
-  
+  currentMillis = millis();
+  ///Light control is down here.
+  if(automode && nextStage <= currentMillis) {
+    //We are in Auto mode.
+    if( curState == offState) {
+        //Go to red.
+        redLight();
+        curState = redState;
+        nextStage = currentMillis + 15000;
+    }else if ( curState == redState) {
+        //Go to green.
+        greenLight();
+        curState = greenState;
+        nextStage = currentMillis + 15000;
+    } else if( curState == greenState ) {
+      //Go to yellow
+       yellowLight();
+       curState = yellowState;
+       nextStage = currentMillis + 4000;
+    } else {
+      //The only state left is the Yellow State.
+      //Go to Red State
+      redLight();
+      curState = redState;
+      nextStage = currentMillis + 15000;
+    }
+  } else if (automode == false && nextStage <= currentMillis) {
+    //Manual Mode. First we check if something is the Queue before we try to do anything.
+  }
 
 }
